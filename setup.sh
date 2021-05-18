9d06ffbef47338ca1372efe5cd6c5db4ab883b19
Ronell Salunke <ronell1292k@gmail.com>
2021-05-18 12:50:53 +0530

    Create a basic Ubuntu/Pop setup script
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Update and upgrade
+echo "Check for updates..."
+sudo apt update && apt list -u && sudo apt upgrade -y
+
+# Install some command line and GUI utilities
+echo "Setting up some utilites and apps..."
+sudo apt install alacritty code curl git gnome-tweaks lm-sensors neofetch ubuntu-restricted-extras vlc wget zsh zsh-autosuggestions -y
+
+# Install OMZSH and PL10K
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ""
+git clone --depth 1 https://github.com/romkatv/powerlevel10k.git /home/$USER/.oh-my-zsh/custom/themes/powerlevel10k
+
+# Download and install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
+rm -rf ./google-chrome-stable_current_amd64.deb
+
+# Setup Git credentials
+echo "Setting up Git credentials..."
+git config --global user.email "ronell1292k@gmail.com"
+git config --global user.name "Ronell Salunke"
+
+# Switch from noveau to integrated graphics via System76 Power extension
+echo "Set graphics to integrated (iGPU) for better battery life..."
+sudo system76-power graphics integrated
+
+# Switch to ZSH and reboot
+echo "Rebooting system..."
+chsh -s /usr/bin/zsh
+sudo reboot
